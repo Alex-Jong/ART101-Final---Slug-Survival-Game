881905f1ecc90ac445c2865959fb989468ae0d8f
Alex-Jong <hawaiiianninja@gmail.com>
2026-05-28 01:48:06 -0700

    Changes woah

--- SlugSurvival.js
+++ SlugSurvival.js
@@ -5,6 +5,10 @@ $(".MidPage").hide();
 $(".lake").hide();
 $(".forest").hide();
 $(".cat").hide();
+$(".downRiver").hide();
+$(".merill").hide();
+$(".barrel").hide();
+$(".barrelAsk").hide();
 
 $("#TvScreen").hover(
   function () {
@@ -45,7 +49,29 @@ $("#continue").click(function () {
     $("#ForestLeftImg").click(function () {
       $("body").css("background-image", 'url(barrels.png');
       $(".forest").hide();
+      $(".barrel").show();
     });
+
+    $("#BarrelDoor").click(function () {
+      ask();
+    });
+
+    function ask() {
+        
+        let user = prompt("Are you sure you want to enter? [Y/N]");
+
+        if (user == "Y") {
+          $("body").css("background-image", 'url(Backrooms.png');
+          $(".barrelAsk").show();
+          $(".barrel").hide();
+          $("#RoomAnswer").html("You gaze upon the unfamiliar place in awe. But awe turns to shock and despair as you turn around and see the door you went through dissapear trapping you forever...");
+          $("#RoomBack").click(function () {
+            $("body").css("background-image", 'url(MidSection1.png');
+            $(".barrelAsk").hide();
+            $(".MidPage").show();
+          });
+        }
+      }
 
     $("#ForestRightImg").click(function () {
       $("body").css("background-image", 'url(cats.png');
@@ -59,7 +85,6 @@ $("#continue").click(function () {
         $(".MidPage").show();
       });
     });
-
   });
 
   // Lake Path
@@ -75,10 +100,32 @@ $("#continue").click(function () {
       $(".lake").hide();
     });
 
-    $("#LakeRightImg").click(function () { });
-
+    $("#LakeRightImg").click(function () {
+      $("body").css("background-image", 'url(DownRiver.png');
+      $(".downRiver").show();
+      $(".lake").hide()
+    });
   });
-});
 
+  // River Up Path
+  $("#RiverUp").click(function () {
+    $("body").css("background-image", 'url(MerillMarket.png');
+    $(".downRiver").hide();
+    $(".merill").show();
+
+    // Merill Clickers
+    $("#explore").click(function () {
+      $("body").css("background-image", 'url(BannanaJoes.png');
+      //$(".").show();
+      $(".merill").hide();
+    });
+
+    //$("#LakeRightImg").click(function () {
+      //$("body").css("background-image", 'url(.png');
+      //$(".").show();
+      //$(".").hide()
+    });
+  });
+//});
 
 // MidPage End
