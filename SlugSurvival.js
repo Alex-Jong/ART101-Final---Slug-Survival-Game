52440d7048169e543d4f41090172a0b762ef4e89
Alex-Jong <hawaiiianninja@gmail.com>
2026-05-27 23:13:01 -0700

    Changes YIPPEE

--- SlugSurvival.js
+++ SlugSurvival.js
@@ -2,6 +2,9 @@
 $("#continue").hide()
 $(".CoverScreen").hide();
 $(".MidPage").hide();
+$(".lake").hide();
+$(".forest").hide();
+$(".cat").hide();
 
 $("#TvScreen").hover(
   function () {
@@ -12,26 +15,69 @@ $("#TvScreen").hover(
   }
 );
 
-$("#TvScreen").click(function(){
-    $("#thought").hide();
-    $("#TvScreen").hide();
-    $("#continue").show();
-    $(".CoverScreen").show();
-// IntroPage End
+$("#TvScreen").click(function () {
+  $("#thought").hide();
+  $("#TvScreen").hide();
+  $("#continue").show();
+  $(".CoverScreen").show();
+  // IntroPage End
 });
 
 // MidPage Start
-$("#continue").click(function(){
-    // MidPage Background Replacement
-    $("body").css("background-image", 'url(MidSection1.png');
-    $(".MidPage").show();
-    // Hide the previous Instruction Screen
-    $("#continue").hide();
-    $(".CoverScreen").hide();
-    // Instruction Screen End
-    // MidPage1 Clickers
-    $("#LeftImg").click(function() {});
-    $("#RightImg").click(function() {});
+$("#continue").click(function () {
+  // MidPage Background Replacement
+  $("body").css("background-image", 'url(MidSection1.png');
+  $(".MidPage").show();
+  // Hide the previous Instruction Screen
+  $("#continue").hide();
+  $(".CoverScreen").hide();
+  // Instruction Screen End
+
+  // MidPage1 Clickers
+
+  // Forest path
+  $("#LeftImg").click(function () {
+    $("body").css("background-image", 'url(forest.png');
+    $(".MidPage").hide();
+    $(".forest").show();
+
+    // Forest Clickers
+    $("#ForestLeftImg").click(function () {
+      $("body").css("background-image", 'url(barrels.png');
+      $(".forest").hide();
+    });
+
+    $("#ForestRightImg").click(function () {
+      $("body").css("background-image", 'url(cats.png');
+      $(".forest").hide();
+      $(".cat").show();
+      $("#catAnswer").html("You feel a sudden surge of gravity, in an instant your body contorts into a grotesque form. You, in an attempt at desperation attempt to cry out for help. Meow! meow?? Herein lies your soul eternally in rest in the gentle embrace of the Cat's Cradle");
+      
+      $("#catBack").click(function () {
+        $("body").css("background-image", 'url(MidSection1.png');
+        $(".cat").hide();
+        $(".MidPage").show();
+      });
+    });
+
+  });
+
+  // Lake Path
+  $("#RightImg").click(function () {
+    $("body").css("background-image", 'url(lake.png');
+    $(".MidPage").hide();
+    $(".lake").show();
+
+    // Lake Clickers
+    $("#LakeLeftImg").click(function () {
+      $("body").css("background-image", 'url(MidSection1.png');
+      $(".MidPage").show();
+      $(".lake").hide();
+    });
+
+    $("#LakeRightImg").click(function () { });
+
+  });
 });
 
 
