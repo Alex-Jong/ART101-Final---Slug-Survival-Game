cc82024aa8f67675766e235982f8777c63752ccf
Alex-Jong <hawaiiianninja@gmail.com>
2026-05-27 17:57:29 -0700

    chnages

--- SlugSurvival.js
+++ SlugSurvival.js
@@ -4,25 +4,35 @@ $("#continue").hide()
 $("#CoverScreen").hide();
 $("#MidPage").hide();
 
+$("#TvScreen").hover(
+  function () {
+    $("#thought").stop(true, true).slideDown(300);
+  },
+  function () {
+    $("#thought").stop(true, true).slideUp(300);
+  }
+);
+
 $("#TvScreen").click(function(){
     $("body").css("background-image", 'url(');
+    $("#thought").hide();
     $("#TvScreen").hide();
     $("#answer").show();
-    $("3continue").show();
+    $("#continue").show();
     $("#CoverScreen").show();
     $("#answer").html("Your objective is to choose a door to go through. Each door will lead to an different path so choose wisely.")
 });
 // IntroPage End
 
 // MidPage Start
-$("continue").click(function(){
+$("#continue").click(function(){
     // MidPage Background Replacement
-    $("body").css("background-image", 'url(');
-    $("MidPage").show();
+    $("body").css("background-image", 'url(MidSection1.png');
+    $("#MidPage").show();
     // Hide the previous Instruction Screen
     $("#answer").hide();
-    $("continue").hide();
-    $("CoverScreen").hide();
+    $("#continue").hide();
+    $("#CoverScreen").hide();
     // Instruction Screen End
 });
 
